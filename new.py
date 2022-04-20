5f81460fb381f0c27b34b87b79f1d0e6de15e1c7
DzmitryKukharenka <103997707+DzmitryKukharenka@users.noreply.github.com>
2022-04-20 17:15:45 +0300

    Delete new.py

--- new.py
+++
@@ -1,8 +0,0 @@
-from pickle import TRUE
-
-
-cort =("шалаш","дом","мама","ротор")
-
-for key in cort:
-    pol = list(filter(lambda key: key == "".join(reversed(key)),cort))
-print("Полиморфы:",pol)
